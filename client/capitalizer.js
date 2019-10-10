91034e58a3661367182048d3e2e8266f58e53431
Dave Trost <davetrost@gmail.com>
2019-10-09 18:18:06 -0700

    file rename

--- client/capitalizer.js
+++ client/capitalizer.js
@@ -1,7 +1,8 @@
 const io = require('socket.io-client');
 const URL = 'http://localhost:7890';
 const socket = io.connect(URL);
+const capitalizeContent = require('../lib/capitalize-content');
 
 socket.on('capitalize-file', message => {
-  socket.emit('file-write', message.toUpperCase());
+  socket.emit('file-write', capitalizeContent(message));
 });
