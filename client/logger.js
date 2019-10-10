1f62b17949d4695242510bc0380b7bdd8e9bb100
Dave Trost <davetrost@gmail.com>
2019-10-10 00:00:21 -0700

    improved logging

--- client/logger.js
+++ client/logger.js
@@ -2,4 +2,7 @@ const io = require('socket.io-client');
 const URL = 'http://localhost:7890';
 const socket = io.connect(URL);
 
-socket.on('log', message => console.log(message));
+socket.on('log', message => {
+  console.log(message.slice(0, 256));
+  if(message.length > 256) console.log('... [message truncated for brevity]');
+});
